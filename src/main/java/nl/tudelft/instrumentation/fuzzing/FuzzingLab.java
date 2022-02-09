5d5569f40f0d032c45a72344aaa2b9c388e78dc8
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-02-09 19:06:14 +0100

    Start work on branch distance

--- src/main/java/nl/tudelft/instrumentation/fuzzing/FuzzingLab.java
+++ src/main/java/nl/tudelft/instrumentation/fuzzing/FuzzingLab.java
@@ -17,12 +17,119 @@ public class FuzzingLab {
                 currentTrace = generateRandomTrace(inputSymbols);
         }
 
+        static int stringDifference(String a, String b) {
+          int index = 0;
+          int difference = 0;
+          while(index < a.length() && index < b.length()){
+            difference += Math.abs(a.charAt(index) - b.charAt(index));
+            index++;
+          }
+          while(index < a.length()){
+            difference += a.charAt(index);
+          }
+          while(index < b.length()){
+            difference += b.charAt(index);
+          }
+          return difference;
+        }
+
+        static int binaryOperatorDistance(MyVar condition, boolean value) {
+          switch (condition.operator) {
+            case "==":
+              if(!value) {
+                if (condition.right.type == TypeEnum.INT && condition.left.type == TypeEnum.INT) {
+                  return Math.abs(condition.right.int_value - condition.left.int_value);
+                } else if(condition.right.type == TypeEnum.STRING && condition.left.type == TypeEnum.STRING){
+                  return stringDifference(condition.left.str_value, condition.right.str_value);
+                }
+              } else {
+                if (condition.right.type == TypeEnum.INT && condition.left.type == TypeEnum.INT) {
+                  return condition.right.int_value == condition.left.int_value? 1 : 0;
+                } else if(condition.right.type == TypeEnum.STRING && condition.left.type == TypeEnum.STRING){
+                  return condition.left.str_value.equals(condition.right.str_value)? 1 : 0;
+                }
+              }
+            case "&&":
+              if (value) {
+                return Math.min(branchDistance(condition.left, value), branchDistance(condition.right, value));
+              } else {
+                return branchDistance(condition.left, value) + branchDistance(condition.right, value);
+              }
+          }
+          System.out.println("not implemented yet, binaryOperatorDistance: " + condition.operator);
+          return 0;
+        }
+
+        static int unaryOperatorDistance(MyVar condition, boolean value) {
+          switch (condition.operator) {
+            case "!":
+              if (condition.left.type == TypeEnum.BOOL) {
+                return value == condition.left.value? 1 : 0;
+              } else {
+                return 1 - branchDistance(condition.left, value);
+              }
+          }
+          return 0;
+        }
+
+        /**
+         * Making a if-statement true: (value = false)
+         a : d = {0 if a is true, 1 otherwise}
+         !a : d = {1 if a is true, 0 otherwise}
+         a == b : d = abs(a-b)
+         a != b : d = {0 if a !=b, 1 otherwise}
+         a < b : d = {0 if a < b; a-b + K otherwise}
+         a <= b : d = {0 if a <= b; a-b otherwise}
+         a > b : d = {0 if a > b; b-a+K otherwise}
+         a >= b : b = {0 if a >= b; b - a otherwise}
+         and for combinations of predicates:
+
+         p1 & p2 : d = d(p1) + d(p2)
+         p1 | p2 : d = min(d(p1), d(p2))
+         p1 XOR p2 : d = min(d(p1) + d(!p2), d(!p1) + d(p2))
+         !p1 : d = 1 - d(p1)
+
+
+         * Making a if-statement false: (value = true)
+
+         a : d = {1 if a is true, 0 otherwise}
+         !a : d = {0 if a is true, 1 otherwise}
+         a == b : d = {0 if a != b, 1 otherwise}
+         a != b : d = abs(a-b)
+         a < b : d = {b-a if a < b; 0 otherwise}
+         a <= b : d = {b-a+1 if a <= b; 0 otherwise}
+         a > b : d = {a-b if a > b; 0 otherwise}
+         a >= b : b = {a-b+1 if a >= b; 0 otherwise}
+
+         and for combinations of predicates:
+         p1 & p2 : d = min(d(p1), d(p2))
+         p1 | p2 : d = d(p1) + d(p2)
+         p1 XOR p2 : d = min(d(p1) + d(p2), d(!p1) + d(!p2))
+         !p1 : d = 1 - d(p1)
+         */
+        static int branchDistance(MyVar condition, boolean value) {
+          switch(condition.type) {
+            case BINARY:
+              return binaryOperatorDistance(condition, value);
+            case BOOL:
+              if(!value) {
+                return condition.value == value? 1 : 0;
+              } else {
+                return condition.value == value? 0 : 1;
+              }
+            default:
+              break;
+          }
+          System.out.println("not implemented yet, branchDistance: " + condition.toString());
+          return 0;
+        }
+
         /**
          * Write your solution that specifies what should happen when a new branch has been found.
          */
         static void encounteredNewBranch(MyVar condition, boolean value, int line_nr) {
                 // do something useful
-                System.out.println(condition.toString());
+                System.out.println(condition.toString() +"\t now "+ value + ".\t Distance: " +branchDistance(condition, value));
         }
 
         /**
