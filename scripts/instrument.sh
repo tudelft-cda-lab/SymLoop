05e4c2569b6d6ed6f30abb7487f41c9c09ff6b53
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-02-17 12:45:57 +0100

    Instrumenting script

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+cd /home/str/JavaInstrumentation
+mkdir -p instrumented
 if [ -z $1 ]; then
-  echo "Please specify an problem number"; 
+  for i in {1..9}; do
+    echo "Instrumenting $i";
+    java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=fuzzing --file="../RERS/Problem$i/Problem$i.java" > "instrumented/Problem$i.java"
+    javac -cp target/aistr.jar:. Errors.java "instrumented/Problem$i.java"
+  done
+  for i in {11..19}; do
+    echo "Instrumenting $i";
+    java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=fuzzing --file="../RERS/Problem$i/Problem$i.java" > "instrumented/Problem$i.java"
+    javac -cp target/aistr.jar:. Errors.java "instrumented/Problem$i.java"
+  done
 else
-  cd /home/str/JavaInstrumentation
   # mvn clean package
-  mkdir -p instrumented
   java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=fuzzing --file="../RERS/Problem$1/Problem$1.java" > "instrumented/Problem$1.java"
   javac -cp target/aistr.jar:. Errors.java "instrumented/Problem$1.java"
   # java -cp target/aistr.jar:./instrumented:. "Problem$1"
