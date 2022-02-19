5cfcf98f04481f3ceebb48e8b0685dc7d7054456
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-02-19 12:38:32 +0100

    Cleanup run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 modified=$(stat -c%Z src/main/java/nl/tudelft/instrumentation/fuzzing/FuzzingLab.java)
 lastmodified=$(cat scripts/lastmodified)
-echo $modified $lastmodified
 if [ "$modified" -gt "$lastmodified" ]; then
   echo $modified > scripts/lastmodified
   mvn clean package
   clear
 fi
-cat scripts/lastmodified
 echo $modified > scripts/lastmodified
 java -cp target/aistr.jar:./instrumented:. Problem$1
