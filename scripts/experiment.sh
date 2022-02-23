d1e0f2c9cb14ee8a4706f34d49e2fd75b6d4cbb4
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-02-23 21:00:23 +0100

    Scripts all the way

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 D=$(date "+%Y.%m.%d-%H.%M.%S")
 set -e
+mkdir -p experiments/$D
 for i in {11..19}; do
   echo -n "$i, "
   # ./scripts/run.sh $i | grep -A1 stable
-  echo "START $i" >> experiments/$D.full.log
-  ./scripts/run.sh $i | tee -a experiments/$D.full.log | tail -n2 | tee -a experiments/$D.log
-  echo "DONE $i" >> experiments/$D.full.log
+  echo "START $i" >> experiments/$D/full.log
+  ./scripts/run.sh $i | tee -a experiments/$D/full.log | tee -a experiments/$D/$i.log | tail -n2 | tee -a experiments/$D/summary.log
+  echo "DONE $i" >> experiments/$D/full.log
   # echo Done problem $i
+  echo "Iter,Visited,Discovered,Errors,Score,TracesPerIter,Tracelength,Time" > experiments/$D/$i.sheet.csv
+  cat experiments/$D/$i.log | grep 'Iter' | grep -o '[0-9]\+\(\.[0-9]\+\)\?' | xargs -n 8 | tr ' ' ',' > experiments/$D/$i.sheet.csv
 done
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,6 @@ set -e
 modified=$(stat -c%Z src/main/java/nl/tudelft/instrumentation/fuzzing/FuzzingLab.java)
 touch scripts/lastmodified
 lastmodified=$(cat scripts/lastmodified)
-echo $modified > scripts/lastmodified
 if [ "$modified" -gt "$lastmodified" ]; then
   mvn clean package
   clear
