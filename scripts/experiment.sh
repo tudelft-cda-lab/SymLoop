bf3d9c9e7cb37b6e4f62845e5bad5b5b4b83ace5
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-02-23 10:54:47 +0100

    Experiment - 1.1

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+D=$(date "+%Y.%m.%d-%H.%M.%S")
 set -e
 for i in {11..19}; do
   echo -n "$i, "
   # ./scripts/run.sh $i | grep -A1 stable
-  ./scripts/run.sh $i | tail -n1
+  echo "START $i" >> experiments/$D.full.log
+  ./scripts/run.sh $i | tee -a experiments/$D.full.log | tail -n2 | tee -a experiments/$D.log
+  echo "DONE $i" >> experiments/$D.full.log
   # echo Done problem $i
 done
