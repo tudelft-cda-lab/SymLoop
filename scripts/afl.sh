22a869e9aae7529ce26dfaca742c48c78b9b2139
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-02-23 21:42:23 +0100

    AFL output dir

--- scripts/afl.sh
+++ scripts/afl.sh
@@ -2,31 +2,32 @@
 set -e
 cd /home/str/JavaInstrumentation
 mkdir -p problems
+OUT=problems
 
 prepare () {
     echo "Copying $1";
-    cp "../RERS/Problem$1/Problem$1.c" problems/
+    cp "../RERS/Problem$1/Problem$1.c" $OUT/
     echo "Modifying $1";
     sed -i 's/extern void __VERIFIER_error(int);/void __VERIFIER_error(int i) {fprintf(stderr, "error_%d ", i);assert(0);}/' "problems/Problem$1.c"
-    sed -i 's/scanf("%d", &input);/int ret = scanf("%d", \&input ); if (ret != 1) return 0;/' "problems/Problem$1.c"
-    mkdir -p problems/$1/tests problems/$1/findings
-    sed -n "s/^.*inputs\[\] = {\s*\(\S*\)}.*$/\1/p" "problems/Problem$1.c" | xargs -n 1 -d , | xargs -I % sh -c "echo % > problems/$1/tests/%.txt && echo >> problems/$1/tests/%.txt"
+    sed -i 's/scanf("%d", &input);/int ret = scanf("%d", \&input ); if (ret != 1) return 0;/' "$OUT/Problem$1.c"
+    mkdir -p $OUT/$1/tests $OUT/$1/findings
+    sed -n "s/^.*inputs\[\] = {\s*\(\S*\)}.*$/\1/p" "$OUT/Problem$1.c" | xargs -n 1 -d , | xargs -I % sh -c "echo % > $OUT/$1/tests/%.txt && echo >> $OUT/$1/tests/%.txt"
     echo "Compiling $1";
-    ../AFL/afl-2.52b/afl-gcc "problems/Problem$1.c" -o "problems/Problem$1"
+    ../AFL/afl-2.52b/afl-gcc "$OUT/Problem$1.c" -o "$OUT/Problem$1"
     echo "Fuzzing $1";
-    AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 ../AFL/afl-2.52b/afl-fuzz -i "problems/$1/tests" -o "problems/$1/findings" "problems/Problem$1"
-    ../AFL/afl-2.52b/afl-plot problems/$1/findings/ problems/$1/plot
+    AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 ../AFL/afl-2.52b/afl-fuzz -i "$OUT/$1/tests" -o "$OUT/$1/findings" "$OUT/Problem$1"
+    ../AFL/afl-2.52b/afl-plot $OUT/$1/findings/ $OUT/$1/plot
 
-    echo > problems/$1/errors.txt
+    echo > $OUT/$1/errors.txt
     set +e
-    for f in $(ls problems/$1/findings/crashes/id*);
+    for f in $(ls $OUT/$1/findings/crashes/id*);
     do
-        cat $f | problems/Problem$1 2>> problems/$1/errors.txt 1> /dev/null
+        cat $f | $OUT/Problem$1 2>> $OUT/$1/errors.txt 1> /dev/null
     done;
     clear
-    cat problems/$1/errors.txt | grep error | sort -V | uniq | tee problems/$1/final.txt
+    cat $OUT/$1/errors.txt | grep error | sort -V | uniq | tee $OUT/$1/final.txt
     set -e
-    echo Found $(cat problems/$1/final.txt | wc -l) distinct errors
+    echo Found $(cat $OUT/$1/final.txt | wc -l) distinct errors
 }
 # for i in {11..19}; do
 # prepare $i
