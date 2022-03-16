3a220d6f6ddd2092b9955419f673e5b9054dbbf7
Wessel <wesselthomas1999@gmail.com>
2022-03-16 15:50:34 +0100

    Add Tarantula calculations and suspicion

--- src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
+++ src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java
@@ -6,15 +6,18 @@ public class PatchingLab {
 
     static Random r = new Random();
     static boolean isFinished = false;
+    static int[] visited;
 
     static void initialize() {
+        visited = new int[OperatorTracker.operators.length];
+
         // initialize the population based on OperatorTracker.operators
     }
 
     // encounteredOperator gets called for each operator encountered while running tests
     static boolean encounteredOperator(String operator, int left, int right, int operator_nr) {
         // Do something useful
-        System.out.print("NUM OP");
+        visited[operator_nr] = 1;
 
         String replacement = OperatorTracker.operators[operator_nr];
         if (replacement.equals("!=")) return left != right;
@@ -28,7 +31,7 @@ public class PatchingLab {
 
     static boolean encounteredOperator(String operator, boolean left, boolean right, int operator_nr) {
         // Do something useful
-        System.out.print("BOOL OP");
+        visited[operator_nr] = 1;
 
         String replacement = OperatorTracker.operators[operator_nr];
         if (replacement.equals("!=")) return left != right;
@@ -36,17 +39,74 @@ public class PatchingLab {
         return false;
     }
 
-    static float basicFitness(List<Boolean> results) {
-        float counter = 0;
+    static double basicFitness(List<Boolean> results) {
+        double counter = 0;
         for (int i = 0; i < results.size(); i++) {
             if (results.get(i) == false) {
                 counter += 1;
             }
         }
 
-        float fitness = counter / (float) results.size();
-        System.out.println("Fitness for this problem is: " + fitness);
+        double fitness = counter / (double) results.size();
         return fitness;
+    }
+
+    static double[] tarantulations() {
+        int amountOperators = OperatorTracker.operators.length;
+        int[] pass = new int[amountOperators];
+        int[] fail = new int[amountOperators];
+        double[] scores = new double[amountOperators];
+        int totalpass = 0;
+        int totalfail = 0;
+
+        int amountTests = OperatorTracker.tests.size();
+        for (int i = 0; i < amountTests; i++) {
+            boolean res = OperatorTracker.runTest(i);
+            if (res) {
+                totalpass++;
+                addArrays(pass, visited);
+            } else {
+                totalfail++;
+                addArrays(fail, visited);
+            }
+        }
+
+        for (int i = 0; i < amountOperators; i++) {
+            double score;
+
+            if (fail[i] == 0 && pass[i] == 0) {
+                score = 1;
+            } else {
+                score = ((double) fail[i] / (double) totalfail) /
+                        (((double) fail[i] / (double) totalfail) + ((double) pass[i] / (double) totalpass));
+            }
+            scores[i] = score;
+        }
+
+        return scores;
+    }
+
+    static void addArrays(int[] a1, int[] a2) {
+        if (a1.length != a2.length) {
+            throw new IllegalArgumentException("Array length not equal!");
+        }
+
+        for (int i = 0; i < a1.length; i++) {
+            a1[i] += a2[i];
+
+            // reset visited while you're already looping through it
+            a2[i] = 0;
+        }
+    }
+
+    static double[] calcSuspicious() {
+        double[] hues = tarantulations();
+        for (int i = 0; i < hues.length; i++) {
+            hues[i] = 1 - hues[i];
+        }
+
+        System.out.println(Arrays.toString(hues));
+        return hues;
     }
 
     static void run() {
@@ -57,8 +117,12 @@ public class PatchingLab {
         // Tests are loaded from resources/tests.txt, make sure you put in the right tests for the right problem!
         List<Boolean> testresults = OperatorTracker.runAllTests();
         System.out.println("Entered run");
+        System.out.println(OperatorTracker.tests.size());
 
-        basicFitness(testresults);
+
+        // basicFitness(testresults);
+
+        double[] sussie = calcSuspicious();
 
         // Loop here, running your genetic algorithm until you think it is done
         while (!isFinished) {
