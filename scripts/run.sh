6f18c2bebee306687a1e62716a5597e89b0796ce
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-03-16 10:39:37 +0100

    Run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-modified=$(stat -c%Z src/main/java/nl/tudelft/instrumentation/symbolic/SymbolicExecutionLab.java)
+modified=$(stat -c%Z src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java)
 touch scripts/lastmodified
 lastmodified=$(cat scripts/lastmodified)
 if [ "$modified" -gt "$lastmodified" ]; then
@@ -8,4 +8,6 @@ if [ "$modified" -gt "$lastmodified" ]; then
   clear
 fi
 echo $modified > scripts/lastmodified
+ln -sf "$PWD/rers2020_test_cases/Problem$1Testcases.txt" src/main/resources/tests.txt
+cat src/main/resources/tests.txt
 java -XX:+UseConcMarkSweepGC -XX:-UseGCOverheadLimit -Xmx4G -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. Problem$1
