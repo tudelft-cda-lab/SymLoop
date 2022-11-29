263c28e7b2865ca3bee0708a9020758e7b65683b
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-11-29 14:44:07 +0100

    Infinite loop program

---
+++ custom_problems/ProblemInfiniteLoop.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ProblemInfiniteLoop {
+    static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+    private String[] inputs = { "i", "p"};
+
+    public int i = 0;
+    public boolean cf = true;
+    public boolean done = false;
+
+    public void calculateOutput(String input) {
+        cf = true;
+        if (cf && input.equals("i")) {
+            i += 1;
+            cf = false;
+        }
+        if (cf && input.equals("p")) {
+            cf = false;
+            if (i > 4) {
+                Errors.__VERIFIER_error(0);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // init system and input reader
+        ProblemInfiniteLoop eca = new ProblemInfiniteLoop();
+
+        // main i/o-loop
+        while (true) {
+            // read input
+            String input = stdin.readLine();
+
+            try {
+                // operate eca engine output =
+                eca.calculateOutput(input);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Invalid input: " + e.getMessage());
+            }
+        }
+    }
+}
