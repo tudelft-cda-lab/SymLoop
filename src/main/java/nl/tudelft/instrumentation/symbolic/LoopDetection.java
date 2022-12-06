6b86794ec936679e6af0cae7e066cc872d4a452c
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-12-06 18:22:54 +0100

    Loop detection of multiple loops

--- src/main/java/nl/tudelft/instrumentation/symbolic/LoopDetection.java
+++ src/main/java/nl/tudelft/instrumentation/symbolic/LoopDetection.java
@@ -14,7 +14,6 @@ public class LoopDetection {
     private HashSet<String> alreadyChecked = new HashSet<>();
     private Context ctx = PathTracker.ctx;
 
-
     private ConstraintHistory history = new ConstraintHistory();
 
     public LoopDetection() {
@@ -63,100 +62,103 @@ public class LoopDetection {
     }
 
     // private Set<BoolExpr> getConstants(List<Replacement> replacements) {
-    //     List<Expr> constantVariables = new ArrayList<Expr>();
-    //     List<Expr> constantValues = new ArrayList<Expr>();
-    //     for (Entry<String, List<Expr>> entry : variables.entrySet()) {
-    //         List<Expr> assigns = entry.getValue();
-    //         String name = entry.getKey();
-    //         int lastLength = getLastVariableLength(name);
-    //         Sort s = assigns.get(0).getSort();
-    //         for (int i = lastLength - 1; i < assigns.size(); i++) {
-    //             Expr e = assigns.get(i);
-    //             if (isConstant(e)) {
-    //                 constantVariables.add(ctx.mkConst(ctx.mkSymbol(SymbolicExecutionLab.getVarName(name, i)), s));
-    //                 constantValues.add(e);
-    //             }
-    //         }
-    //     }
+    // List<Expr> constantVariables = new ArrayList<Expr>();
+    // List<Expr> constantValues = new ArrayList<Expr>();
+    // for (Entry<String, List<Expr>> entry : variables.entrySet()) {
+    // List<Expr> assigns = entry.getValue();
+    // String name = entry.getKey();
+    // int lastLength = getLastVariableLength(name);
+    // Sort s = assigns.get(0).getSort();
+    // for (int i = lastLength - 1; i < assigns.size(); i++) {
+    // Expr e = assigns.get(i);
+    // if (isConstant(e)) {
+    // constantVariables.add(ctx.mkConst(ctx.mkSymbol(SymbolicExecutionLab.getVarName(name,
+    // i)), s));
+    // constantValues.add(e);
+    // }
+    // }
+    // }
 
-    //     Expr[] constantVariablesArray = constantVariables.toArray(Expr[]::new);
-    //     Expr[] constantValueArray = constantValues.toArray(Expr[]::new);
+    // Expr[] constantVariablesArray = constantVariables.toArray(Expr[]::new);
+    // Expr[] constantValueArray = constantValues.toArray(Expr[]::new);
 
-    //     Set<BoolExpr> baseConstraints = loopModelList.stream().map(e -> {
-    //         return (BoolExpr) e.substitute(constantVariablesArray, constantValueArray);
-    //     }).map(e -> {
-    //         BoolExpr n = e;
-    //         for (Replacement r : replacements) {
-    //             n = r.applyTo(n);
-    //         }
-    //         if (!n.equals(e)) {
-    //             return n;
-    //         } else {
-    //             return null;
-    //         }
-    //     }).filter(Objects::nonNull).collect(Collectors.toSet());
-    //     return baseConstraints;
+    // Set<BoolExpr> baseConstraints = loopModelList.stream().map(e -> {
+    // return (BoolExpr) e.substitute(constantVariablesArray, constantValueArray);
+    // }).map(e -> {
+    // BoolExpr n = e;
+    // for (Replacement r : replacements) {
+    // n = r.applyTo(n);
+    // }
+    // if (!n.equals(e)) {
+    // return n;
+    // } else {
+    // return null;
+    // }
+    // }).filter(Objects::nonNull).collect(Collectors.toSet());
+    // return baseConstraints;
     // }
 
     boolean isLoopDone() {
         if (SymbolicExecutionLab.skip) {
             return false;
         }
-        int lastNSaves = 1;
+        history.save();
+        int lastNSaves = 2;
         if (history.getNumberOfSaves() < lastNSaves) {
             history.save();
             return true;
         }
-        BoolExpr loopModel = history.getConstraint(lastNSaves);
-        BoolExpr extended = loopModel;
-        List<Replacement> replacements = history.getReplacementsForLastSaves(lastNSaves);
+        int MAX_LOOP_DETECTION_DEPTH = 2;
 
-        for (Replacement r : replacements) {
-            extended = r.applyTo(extended);
-        }
+        for (; lastNSaves < history.getNumberOfSaves() && lastNSaves <= MAX_LOOP_DETECTION_DEPTH+1; lastNSaves++) {
+            BoolExpr loopModel = history.getConstraint(lastNSaves);
+            BoolExpr extended = loopModel;
+            List<Replacement> replacements = history.getReplacementsForLastSaves(lastNSaves);
 
-        history.save();
-
-        if (replacements.size() > 0 && alreadyChecked.add(SymbolicExecutionLab.processedInput)
-                && PathTracker.solve(extended, false, false)
-                && !foundLoops.contains(SymbolicExecutionLab.processedInput)) {
-            foundLoops.add(SymbolicExecutionLab.processedInput);
-            foundLoops.sort(String::compareTo);
-
-            if (isSelfLoop(replacements, extended)) {
-                return false;
+            for (Replacement r : replacements) {
+                extended = r.applyTo(extended);
             }
 
-            String output = replacements.stream().map(Replacement::getName).collect(Collectors.joining(", "));
+            if (replacements.size() > 0 && alreadyChecked.add(lastNSaves+'-'+SymbolicExecutionLab.processedInput)
+                    && PathTracker.solve(extended, false, false)
+                    && !foundLoops.contains(SymbolicExecutionLab.processedInput)) {
+                foundLoops.add(SymbolicExecutionLab.processedInput);
+                foundLoops.sort(String::compareTo);
 
-            SymbolicExecutionLab.printfRed(
-                    "loop detected with vars %s: on input '%s'. %d / %d constraints need updating\n", output,
-                    SymbolicExecutionLab.processedInput,
-                    -1, -1);
-            SymbolicExecutionLab.printfBlue("loopModel: %s\n", loopModel);
-
-
-            Solver solver = ctx.mkSolver();
-            solver.add(PathTracker.z3model);
-            solver.add(PathTracker.z3branches);
-            solver.add(extended);
-            for (int i = 1; i < 100; i += 1) {
-                for (Replacement r : replacements) {
-                    extended = r.applyTo(extended, i);
+                if (isSelfLoop(replacements, extended)) {
+                    return false;
                 }
+
+                String output = replacements.stream().map(Replacement::getName).collect(Collectors.joining(", "));
+
+                SymbolicExecutionLab.printfRed(
+                        "loop detected over %d iterations with vars %s: on input '%s'. \n", lastNSaves -1, output,
+                        SymbolicExecutionLab.processedInput);
+                // SymbolicExecutionLab.printfBlue("loopModel: %s\n", loopModel);
+
+                Solver solver = ctx.mkSolver();
+                solver.add(PathTracker.z3model);
+                solver.add(PathTracker.z3branches);
                 solver.add(extended);
-                Status status = solver.check();
-                if (status == Status.UNSATISFIABLE) {
-                    SymbolicExecutionLab.printfGreen("loop ends with %s, after %d iterations on model %s\n", status, i, extended);
-                    return true;
-                } else if (status == Status.UNKNOWN){
-                    SymbolicExecutionLab.printfRed("Solver exited with status: %s\n", status);
-                    System.exit(1);
+                for (int i = 1; i < 100; i += 1) {
+                    for (Replacement r : replacements) {
+                        extended = r.applyTo(extended, i);
+                    }
+                    solver.add(extended);
+                    Status status = solver.check();
+                    if (status == Status.UNSATISFIABLE) {
+                        SymbolicExecutionLab.printfGreen("loop ends with %s, after %d iterations on model %s\n", status,
+                                i, extended);
+                        return true;
+                    } else if (status == Status.UNKNOWN) {
+                        SymbolicExecutionLab.printfRed("Solver exited with status: %s\n", status);
+                        System.exit(1);
+                    }
                 }
-            }
 
-            for (String s : foundLoops) {
-                SymbolicExecutionLab.printfBlue("%s\n", s);
+                for (String s : foundLoops) {
+                    SymbolicExecutionLab.printfBlue("%s\n", s);
+                }
             }
         }
         return true;
