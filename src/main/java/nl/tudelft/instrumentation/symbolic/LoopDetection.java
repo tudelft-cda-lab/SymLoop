e36389d80837e82e2cfcf032c214705ab57c5d3d
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-12-13 14:20:14 +0100

    Format loopdetection code

--- src/main/java/nl/tudelft/instrumentation/symbolic/LoopDetection.java
+++ src/main/java/nl/tudelft/instrumentation/symbolic/LoopDetection.java
@@ -10,12 +10,11 @@ import com.microsoft.z3.*;
 
 public class LoopDetection {
 
-    private static final int LOOP_UNROLLING_AMOUNT = 100;
-    private static final int MAX_LOOP_DETECTION_DEPTH = 3;
+    private static final int LOOP_UNROLLING_AMOUNT = 20;
+    private static final int MAX_LOOP_DETECTION_DEPTH = 1;
 
-    private List<String> foundLoops = new ArrayList<>();
-    private List<String> selfLoops = new ArrayList<>();
-    private HashSet<String> alreadyChecked = new HashSet<>();
+    private SortedSet<String> foundLoops = new TreeSet<>();
+    private SortedSet<String> selfLoops = new TreeSet<>();
     private Context ctx = PathTracker.ctx;
     private String inputName = "unknown";
 
@@ -32,7 +31,6 @@ public class LoopDetection {
         for (Pattern p : patterns) {
             Matcher m = p.matcher(input);
             if (m.find()) {
-                // System.out.printf("loop: %s\n", loop);
                 return true;
             }
         }
@@ -73,60 +71,17 @@ public class LoopDetection {
             all = ctx.mkAnd(r.isSelfLoopExpr(), all);
         }
         if (PathTracker.solve(ctx.mkAnd(all, extended), false, false)) {
-            System.out.println(all);
             return true;
         }
         return false;
     }
 
-    // private Set<BoolExpr> getConstants(List<Replacement> replacements) {
-    // List<Expr> constantVariables = new ArrayList<Expr>();
-    // List<Expr> constantValues = new ArrayList<Expr>();
-    // for (Entry<String, List<Expr>> entry : variables.entrySet()) {
-    // List<Expr> assigns = entry.getValue();
-    // String name = entry.getKey();
-    // int lastLength = getLastVariableLength(name);
-    // Sort s = assigns.get(0).getSort();
-    // for (int i = lastLength - 1; i < assigns.size(); i++) {
-    // Expr e = assigns.get(i);
-    // if (isConstant(e)) {
-    // constantVariables.add(ctx.mkConst(ctx.mkSymbol(SymbolicExecutionLab.getVarName(name,
-    // i)), s));
-    // constantValues.add(e);
-    // }
-    // }
-    // }
-
-    // Expr[] constantVariablesArray = constantVariables.toArray(Expr[]::new);
-    // Expr[] constantValueArray = constantValues.toArray(Expr[]::new);
-
-    // Set<BoolExpr> baseConstraints = loopModelList.stream().map(e -> {
-    // return (BoolExpr) e.substitute(constantVariablesArray, constantValueArray);
-    // }).map(e -> {
-    // BoolExpr n = e;
-    // for (Replacement r : replacements) {
-    // n = r.applyTo(n);
-    // }
-    // if (!n.equals(e)) {
-    // return n;
-    // } else {
-    // return null;
-    // }
-    // }).filter(Objects::nonNull).collect(Collectors.toSet());
-    // return baseConstraints;
-    // }
-    //
-    //
     Pattern getSelfLoopPattern(String input, int lastN) {
         int loopIndex = input.length() - lastN;
         String basePart = input.substring(0, loopIndex);
         String loopPart = input.substring(loopIndex);
         String regex = String.format("^%s(%s)+", basePart, loopPart);
-        System.out.printf("%d, %d:'%s' + '%s', '%s'\n", lastN, loopIndex, basePart, loopPart, regex);
         if (lastN > 1) {
-            // String end = "(<)?";
-            // String end = "((<)(-)?)?";
-            // String end = "(((<)(-)?)?)";
             String end = "";
             List<String> s = new ArrayList<>();
             for (int i = 1; i < lastN; i++) {
@@ -142,19 +97,6 @@ public class LoopDetection {
         return p;
     }
 
-    // String getRegexFor(String input, int lastN, int max_amount) {
-    // int loopIndex = input.length() - lastN;
-    // String basePart = input.substring(0, loopIndex);
-    // String loopPart = input.substring(loopIndex);
-    // String regex = String.format("%s(%s){0,%d}", basePart, loopPart, max_amount);
-    // System.out.printf("%d, %d:'%s' + '%s', '%s'\n", lastN, loopIndex, basePart,
-    // loopPart, regex);
-    // Pattern p = Pattern.compile(regex);
-    // System.exit(1);
-    // return "";
-    // // ,max_amount)
-    // }
-
     boolean isIterationLooping() {
         final String INPUT = SymbolicExecutionLab.processedInput;
         history.save();
@@ -167,14 +109,13 @@ public class LoopDetection {
         }
 
         if (currentPattern != null) {
-            System.out.println(INPUT);
             Matcher m = currentPattern.matcher(INPUT);
             boolean isFullMatch = m.matches();
             if (isFullMatch) {
-                System.out.printf("Still part of current pattern '%s'\n", currentPattern);
+                System.out.printf("'%s' is still part of current pattern '%s'\n", INPUT, currentPattern);
                 return false;
             } else {
-                System.out.printf("Not part of current pattern '%s' %d\n", currentPattern, INPUT.length());
+                System.out.printf("'%s' not part of current pattern '%s' %d\n", INPUT, currentPattern, INPUT.length());
             }
         }
         if (isSelfLooping(INPUT)) {
@@ -196,7 +137,6 @@ public class LoopDetection {
             }
 
             foundLoops.add(SymbolicExecutionLab.processedInput);
-            foundLoops.sort(String::compareTo);
 
             if (isSelfLoop(replacements, extended)) {
                 SymbolicExecutionLab.printfRed("SELF LOOP DETECTED for %s over %d\n",
@@ -224,20 +164,24 @@ public class LoopDetection {
             if (!isLooping(INPUT, loopPatterns)) {
                 loopPatterns.add(currentPattern);
             }
-            List<Pattern> s = new ArrayList<>(loopPatterns);
-            Collections.sort(s, Comparator.comparing(Pattern::toString));
-            for (Pattern p : s) {
-                SymbolicExecutionLab.printfBlue("loop: %s\n", p);
-            }
-
-            s = new ArrayList<>(selfLoopPatterns);
-            Collections.sort(s, Comparator.comparing(Pattern::toString));
-            for (Pattern p : s) {
-                SymbolicExecutionLab.printfBlue("selfloop: %s\n", p);
-            }
+            printLoops();
             return false;
         }
         return false;
+    }
+
+    void printLoops() {
+        List<Pattern> s = new ArrayList<>(loopPatterns);
+        Collections.sort(s, Comparator.comparing(Pattern::toString));
+        for (Pattern p : s) {
+            SymbolicExecutionLab.printfBlue("loop: %s\n", p);
+        }
+
+        s = new ArrayList<>(selfLoopPatterns);
+        Collections.sort(s, Comparator.comparing(Pattern::toString));
+        for (Pattern p : s) {
+            SymbolicExecutionLab.printfBlue("selfloop: %s\n", p);
+        }
     }
 
     boolean isFiniteLoop(BoolExpr extended, List<Replacement> replacements) {
@@ -266,7 +210,8 @@ public class LoopDetection {
         }
 
         List<BoolExpr> onLoop = new ArrayList<>();
-        ArithExpr numberOfTimesTheLoopExecutes = (ArithExpr) ctx.mkConst("custom_loop_number_" + (currentLoopNumber++), ctx.getIntSort());
+        ArithExpr numberOfTimesTheLoopExecutes = (ArithExpr) ctx.mkConst("custom_loop_number_" + (currentLoopNumber++),
+                ctx.getIntSort());
         for (int i = 0; i < LOOP_UNROLLING_AMOUNT; i += 1) {
             List<BoolExpr> thisIteration = new ArrayList<>();
             for (Replacement r : replacements) {
