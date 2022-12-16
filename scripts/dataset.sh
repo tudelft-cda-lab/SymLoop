7106405158c709bee7176882da40a91be17c86d8
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-12-16 16:39:30 +0100

    USE RERS-2020 AND DONT LOOK BACK IN ANGER

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,8 +1,6 @@
 mkdir -p RERS
 cd RERS
-wget https://rers-challenge.org/2019/problems/sequential/SeqReachabilityRers2019_Mar_9th.zip
-wget https://rers-challenge.org/2019/problems/sequential/SeqLtlRers2019_Mar_9th.zip
-ls *.zip | xargs -L 1 unzip
-mv Seq*/Problem* .
-rm -r $(ls | grep -v Problem)
-cd ../
+wget https://www.rers-challenge.org/2020/problems/sequential/SeqReachabilityRers2020.zip
+# wget https://www.rers-challenge.org/2020/problems/sequential/SeqReachabilityRers2020.zip
+ls *.zip | xargs -L 1 unzip -o
+rm -f *.zip
