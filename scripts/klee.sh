86692fa8f433fa1d6c507cc0331d1e3d95c66695
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-12-21 10:14:35 +0100

    Remove old way of doing symbolic input

--- scripts/klee.sh
+++ scripts/klee.sh
@@ -37,7 +37,6 @@ prepare () {
         void __VERIFIER_error(int i) { fprintf(stderr, "error_%d\\n", i); fflush(stderr); assert(0); }/' "$OUT/Problem$1.c"
     # Remove any output
     sed -i '/printf("/d' "$OUT/Problem$1.c"
-    # sed -i -z 's/while(1)\n.*if\(([^\n]*)\)[^}]*}/int length = 1000;int program[length];klee_make_symbolic(program, sizeof(program), "program");for (int i = 0; i < length; ++i) {int input = program[i];if(\1){return 0;}calculate_output(input);}/' "$OUT/Problem$1.c"
     sed -i -z 's/while(1)\n.*if\(([^\n]*)\)[^}]*}/while(1) {int input = 0; klee_make_symbolic(\&input, sizeof(input), "input"); klee_assume(!(\1)); if(\1){return 0;}calculate_output(input);}/' "$OUT/Problem$1.c"
     sed -i '/klee_assume/s/\&\&/\&/' "$OUT/Problem$1.c"
 
