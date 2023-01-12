68bb60dc81fa5bf524af55fa5f2d6a2bdda8e83f
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-01-12 09:04:06 +0100

    Also download LTL problems

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,6 +1,6 @@
 mkdir -p RERS
 cd RERS
 wget https://www.rers-challenge.org/2020/problems/sequential/SeqReachabilityRers2020.zip
-# wget https://www.rers-challenge.org/2020/problems/sequential/SeqReachabilityRers2020.zip
+wget https://rers-challenge.org/2020/problems/sequential/SeqLtlRers2020.zip
 ls *.zip | xargs -L 1 unzip -o
 rm -f *.zip
