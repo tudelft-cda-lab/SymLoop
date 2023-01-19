853d3c01609d3c64e72c29c81c39dbadbf84153a
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-01-19 08:28:53 +0100

    Only build if the timestamps are updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 ln -sf "$PWD/rers2020_test_cases/Problem$1Testcases.txt" src/main/resources/tests.txt
-modified=$(stat -c%Z src/main/java/nl/tudelft/instrumentation/patching/PatchingLab.java)
-touch scripts/lastmodified
-lastmodified=$(cat scripts/lastmodified)
-#if [ "$modified" -gt "$lastmodified" ]; then
-  # mvn -q clean package -
-  mvn -q clean package -DskipTests
-  clear
-#fi
+modified=$(find src/ | grep src/main/java | xargs stat -c "%Y" | sort -n | tail -n 1)
+echo "modified: $modified"
+lastmodified=$(cat scripts/lastmodified || echo "0")
+echo "lastmodified: $lastmodified"
+echo "modified:" $modified
+if [ "$modified" -gt "$lastmodified" ]; then
+   # mvn -q clean package -
+   mvn -q clean package -DskipTests
+  # clear
+fi
 echo $modified > scripts/lastmodified
 
 # java -ea -XX:-UseGCOverheadLimit -Xmx4G -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. Problem$1 ${@:2}
