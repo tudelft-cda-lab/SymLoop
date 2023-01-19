48fa7fc8739863c3fcfcbe4b5b9e8fb260f56a35
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-01-19 07:25:59 +0100

    Add div by zero testing to free_var_z3

--- scripts/free_var_z3.py
+++ scripts/free_var_z3.py
@@ -18,3 +18,12 @@ print("free after: ", check_free(y))
 import ctypes
 a = ctypes.c_ulong(-1)
 b = ctypes.c_ulong(1)
+
+
+print(f'division by zero')
+s = Solver()
+s.add(x == 0)
+s.add(y / x == 1)
+print(s)
+print(s.check())
+print(s.model())
