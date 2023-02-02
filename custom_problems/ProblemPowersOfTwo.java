4bdca457d209d7082b19373e9bd9289e87e8014b
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-02-02 14:54:08 +0100

    Update ProblemPowersOfTwo

--- custom_problems/ProblemPowersOfTwo.java
+++ custom_problems/ProblemPowersOfTwo.java
@@ -18,36 +18,36 @@ public class ProblemPowersOfTwo {
         }
         if (cf && input.equals("p")) {
             cf = false;
-            if (i > 1073741824) { Errors.__VERIFIER_error(30); }
-            if (i > 536870912) { Errors.__VERIFIER_error(29); }
-            if (i > 268435456) { Errors.__VERIFIER_error(28); }
-            if (i > 134217728) { Errors.__VERIFIER_error(27); }
-            if (i > 67108864) { Errors.__VERIFIER_error(26); }
-            if (i > 33554432) { Errors.__VERIFIER_error(25); }
-            if (i > 16777216) { Errors.__VERIFIER_error(24); }
-            if (i > 8388608) { Errors.__VERIFIER_error(23); }
-            if (i > 4194304) { Errors.__VERIFIER_error(22); }
-            if (i > 2097152) { Errors.__VERIFIER_error(21); }
-            if (i > 1048576) { Errors.__VERIFIER_error(20); }
-            if (i > 524288) { Errors.__VERIFIER_error(19); }
-            if (i > 262144) { Errors.__VERIFIER_error(18); }
-            if (i > 131072) { Errors.__VERIFIER_error(17); }
-            if (i > 65536) { Errors.__VERIFIER_error(16); }
-            if (i > 32768) { Errors.__VERIFIER_error(15); }
-            if (i > 16384) { Errors.__VERIFIER_error(14); }
-            if (i > 8192) { Errors.__VERIFIER_error(13); }
-            if (i > 4096) { Errors.__VERIFIER_error(12); }
-            if (i > 2048) { Errors.__VERIFIER_error(11); }
-            if (i > 1024) { Errors.__VERIFIER_error(10); }
-            if (i > 512) { Errors.__VERIFIER_error(9); }
-            if (i > 256) { Errors.__VERIFIER_error(8); }
-            if (i > 128) { Errors.__VERIFIER_error(7); }
-            if (i > 64) { Errors.__VERIFIER_error(6); }
-            if (i > 32) { Errors.__VERIFIER_error(5); }
-            if (i > 16) { Errors.__VERIFIER_error(4); }
-            if (i > 8) { Errors.__VERIFIER_error(3); }
-            if (i > 4) { Errors.__VERIFIER_error(2); }
-            if (i > 2) { Errors.__VERIFIER_error(1); }
+            if (i == 1073741824) { Errors.__VERIFIER_error(30); }
+            if (i == 536870912) { Errors.__VERIFIER_error(29); }
+            if (i == 268435456) { Errors.__VERIFIER_error(28); }
+            if (i == 134217728) { Errors.__VERIFIER_error(27); }
+            if (i == 67108864) { Errors.__VERIFIER_error(26); }
+            if (i == 33554432) { Errors.__VERIFIER_error(25); }
+            if (i == 16777216) { Errors.__VERIFIER_error(24); }
+            if (i == 8388608) { Errors.__VERIFIER_error(23); }
+            if (i == 4194304) { Errors.__VERIFIER_error(22); }
+            if (i == 2097152) { Errors.__VERIFIER_error(21); }
+            if (i == 1048576) { Errors.__VERIFIER_error(20); }
+            if (i == 524288) { Errors.__VERIFIER_error(19); }
+            if (i == 262144) { Errors.__VERIFIER_error(18); }
+            if (i == 131072) { Errors.__VERIFIER_error(17); }
+            if (i == 65536) { Errors.__VERIFIER_error(16); }
+            if (i == 32768) { Errors.__VERIFIER_error(15); }
+            if (i == 16384) { Errors.__VERIFIER_error(14); }
+            if (i == 8192) { Errors.__VERIFIER_error(13); }
+            if (i == 4096) { Errors.__VERIFIER_error(12); }
+            if (i == 2048) { Errors.__VERIFIER_error(11); }
+            if (i == 1024) { Errors.__VERIFIER_error(10); }
+            if (i == 512) { Errors.__VERIFIER_error(9); }
+            if (i == 256) { Errors.__VERIFIER_error(8); }
+            if (i == 128) { Errors.__VERIFIER_error(7); }
+            if (i == 64) { Errors.__VERIFIER_error(6); }
+            if (i == 32) { Errors.__VERIFIER_error(5); }
+            if (i == 16) { Errors.__VERIFIER_error(4); }
+            if (i == 8) { Errors.__VERIFIER_error(3); }
+            if (i == 4) { Errors.__VERIFIER_error(2); }
+            if (i == 2) { Errors.__VERIFIER_error(1); }
         }
     }
 
