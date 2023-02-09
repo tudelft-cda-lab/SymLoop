6f6ffde6bf6123406966edc0508a60901a798230
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-02-09 08:02:32 +0100

    Use clean packacge in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,12 +5,13 @@ modified=$(find src/ | grep src/main/java | xargs stat -c "%Y" | sort -n | tail 
 lastmodified=$(cat scripts/lastmodified || echo "0")
 if [ "$modified" -gt "$lastmodified" ]; then
    # mvn -q clean package -
-   mvn -q -T1C --offline package -DskipTests
+   mvn clean -q -T1C --offline package -DskipTests
+   echo $modified > scripts/lastmodified
   # clear
 else
    echo "SKIPPING BUILD"
 fi
-echo $modified > scripts/lastmodified
+
 
 # java -ea -XX:-UseGCOverheadLimit -Xmx4G -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. Problem$1 ${@:2}
 java -ea -agentpath:$HOME/projects/async-profiler/build/libasyncProfiler.so=start,event=cpu,file=profile.html,title="Problem$1 $(date '+%T')",minwidth=0.2 -XX:-UseGCOverheadLimit -Xmx4G -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. Problem$1 ${@:2}
