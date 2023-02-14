9d6dd38a533613f3d8f72353137ee32f226c1b3a
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-02-14 18:58:11 +0100

    Problem Thesis, the basis of my thesis

---
+++ custom_problems/ProblemThesis.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ProblemThesis {
+    static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+    private String[] inputs = { "i", "p" };
+    private final int LIMIT = Integer.parseInt(System.getenv("LIMIT"));
+
+    public int i = 0;
+    public boolean cf = true;
+    public boolean done = false;
+
+    public void calculateOutput(String input) {
+        cf = true;
+        if (cf && input.equals("i")) {
+            i+=1;
+            cf = false;
+        }
+        if (cf && input.equals("p")) {
+            cf = false;
+            if (i == LIMIT) {
+                Errors.__VERIFIER_error(LIMIT);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // init system and input reader
+        ProblemThesis eca = new ProblemThesis();
+
+        // main i/o-loop
+        while (true) {
+            // read input
+            String input = stdin.readLine();
+
+            try {
+                // operate eca engine output =
+                eca.calculateOutput(input);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Invalid input: " + e.getMessage());
+            }
+        }
+    }
+}
