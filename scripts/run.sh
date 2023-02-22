5e0162d37829ebc628aea17ec7074b0d945f46a1
BramVerb <33070319+BramVerb@users.noreply.github.com>
2023-02-22 10:25:18 +0100

    Faster compilation

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,8 @@ modified=$(find src/ | grep src/main/java | xargs stat -c "%Y" | sort -n | tail 
 lastmodified=$(cat scripts/lastmodified || echo "0")
 if [ "$modified" -gt "$lastmodified" ]; then
    # mvn -q clean package -
-   mvn clean -q -T1C --offline package -DskipTests
+   # mvn clean -q -T1C --offline package -DskipTests
+   mvn compile -q
    echo $modified > scripts/lastmodified
   # clear
 else
@@ -13,5 +14,6 @@ else
 fi
 
 
-java -ea -XX:-UseGCOverheadLimit -Xmx4G -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. Problem$1 ${@:2}
+java -ea -XX:-UseGCOverheadLimit -Xmx4G -cp "target/classes:$(cat .classpath):lib/com.microsoft.z3.jar:./instrumented:." Problem$1 ${@:2}
+# echo "$(cat .classpath):lib/com.microsoft.z3.jar:$PWD/instrumented:." Problem$1 ${@:2}
 # java -ea -agentpath:$HOME/projects/async-profiler/build/libasyncProfiler.so=start,event=cpu,file=profile.html,title="Problem$1 $(date '+%T')",minwidth=0.2 -XX:-UseGCOverheadLimit -Xmx4G -cp target/aistr.jar:lib/com.microsoft.z3.jar:./instrumented:. Problem$1 ${@:2}
